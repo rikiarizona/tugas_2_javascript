05eec0d8f5029b2f944d9cac6df5efb62abf969a
rikiarizona <rikiarizona@gmail.com>
2022-08-03 09:28:30 +0700

    TUGAS 2 JAVASCRIPT
    SAYA KIRIMKAN TUGAS JAVASCRIPT KEPADA NIOMIC

---
+++ index.js
@@ -0,0 +1,22 @@
+var number = 3;
+
+var string = 'kutiptunggal';
+
+var boolean = 20;
+if(boolean>50)
+boolean = true;
+else
+boolean = false;
+
+var tidakada1 = "ada";
+tidakada1 = null;
+
+var tidakada2;
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(tidakada1);
+console.log(tidakada2);
+
+//Happy Coding :)
